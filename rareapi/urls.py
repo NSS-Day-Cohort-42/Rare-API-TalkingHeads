cf85feaa2b54a4ce9fb5d933867ff24a902c6653
Brett Derrington <brett.derrington@gmail.com>
2020-11-12 15:35:09 -0600

    Finished Get, Post, and Delete for posttags

--- rareapi/urls.py
+++ rareapi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 
-from rareserverapi.views import Categories, Posts, register_user, login_user, Tags
+from rareserverapi.views import Categories, Posts, register_user, login_user, Tags, PostTags
 from rareserverapi.views import Comments
 from django.conf.urls import include
 
@@ -10,6 +10,7 @@ router.register(r'categories', Categories, 'category')
 router.register(r'comments', Comments, 'comment')
 router.register(r'posts', Posts, 'post')
 router.register(r'tags', Tags, 'tag')
+router.register(r'posttags', PostTags, 'posttag')
 
 urlpatterns = [
     path('', include(router.urls)),
--- rareserverapi/views/__init__.py
+++ rareserverapi/views/__init__.py
@@ -4,3 +4,4 @@ from .auth import register_user
 from .auth import login_user
 from .comment import Comments
 from .tag import Tags
+from .posttag import PostTags
