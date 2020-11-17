3e8c7ed88f29c3ecdead560f8f1787f4f910ce74
Pete Stewart <petestew@gmail.com>
2020-11-17 09:43:31 -0600

    non-admin token now forbidden from post approvals

--- rareserverapi/views/post.py
+++ rareserverapi/views/post.py
@@ -129,12 +129,20 @@ class Posts(ViewSet):
     def partial_update(self, request, pk=None):
         """partially updates post"""
 
+        user = RareUser.objects.get(user=request.auth.user)
+
         post = Post.objects.get(pk=pk)
         post.approved = request.data["approved"]
 
-        post.save()
+        if user.user.is_staff == True:
+            try:
+                post.save()
+                return Response({}, status=status.HTTP_204_NO_CONTENT)
+            except ValidationError as ex:
+                return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"reason": "user is not an administrator"}, status=status.HTTP_403_FORBIDDEN)
 
     def destroy(self, request, pk=None):
         """Handles DELETE resquests for a post
